726d26f7eaf5cc53540f1c6c41e98aaeec499be8
adeniji4 <128931940+adeniji4@users.noreply.github.com>
2025-05-10 02:43:31 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
           
           <div className="absolute inset-0 flex flex-col items-center justify-center text-white p-4">
             {item.showLogo && <div className="mb-8 animate-fade-in">
-                <img src="./public/Photos/b4196b07-03dc-40ee-a6b4-34e891c1b9bd.png" alt="Mio's Hospitality & C0" className="h-64 w-auto object-contain filter brightness-125" />
+                <img src="/public/Photos/b4196b07-03dc-40ee-a6b4-34e891c1b9bd.png" alt="Mio's Hospitality & C0" className="h-64 w-auto object-contain filter brightness-125" />
               </div>}
             <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center max-w-4xl animate-fade-in">
               {item.title}
